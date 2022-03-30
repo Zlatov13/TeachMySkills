aaa631a5f366d200c2c9132c4e56ba2cc178e242
Zolotukha Ruslan <93220233+Zlatov13@users.noreply.github.com>
2022-03-30 21:54:55 +0300

    1

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        int massive[][] = new int[5][5];
+        for (int i = 0; i < massive.length; i++) {
+            for (int j = 0; j < massive[i].length; j++) {
+                massive[i][j] = (int) (Math.random() * 100);
+                System.out.print(massive[i][j] + " ");
+            }
+            System.out.println();
+            Arrays.sort(massive[i]);
+            for (int y = 0; y < massive[i].length; y++) {
+                System.out.print(massive[i][y] + " ");
+            }
+            System.out.println();
+            for (int z = massive[i].length - 1; z >= 0; z--) {
+                System.out.print(massive[i][z] + " ");
+            }
+            System.out.println("\n");
+        }
+    }
+}
